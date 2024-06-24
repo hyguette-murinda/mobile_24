88c615129a8c098067d4660aae6756292b09a668
hyguette-murinda <hyguettemurindaimfura@gmail.com>
2024-06-24 13:38:09 +0400

    another slight change

--- frontend/app/(auth)/login.jsx
+++ frontend/app/(auth)/login.jsx
@@ -49,14 +49,14 @@ const Login = () => {
 
   }
   return (
-    <SafeAreaView className="bg-[#3366cc] h-full">
+    <SafeAreaView className="bg-[#3366cc] h-full ">
       <ScrollView
         contentContainerStyle={{
           height: "100%",
           marginTop: 100,
         }}
       >
-        <View className="h-full bg-white w-full rounded-t-[25px] flex items-center px-5 py-6">
+        <View className="h-full bg-white w-full rounded-t-[25px] flex items-center px-5 py-[15%] mt-[25%]">
           <TouchableOpacity
             onPress={() => {
               router.push("/home");
@@ -74,7 +74,7 @@ const Login = () => {
             <Text className="text-third">Sign In to continue</Text>
           </View>
           <View className="flex flex-col w-full">
-            <View className="flex flex-row items-center justify-between border w-full h-[50px] rounded-md border-third p-4">
+            <View className="flex flex-row items-center justify-between border w-full h-[50px] rounded-md border-third p-4 ">
               <MaterialCommunityIcons
                 name="email-outline"
                 size={18}
@@ -87,7 +87,7 @@ const Login = () => {
                 className="flex-1 px-3 items-center h-[50px]"
               />
             </View>
-            <View className="flex flex-row items-center mt-2 justify-between border w-full h-[50px] rounded-md border-third overflow-hidden py-4 pl-4">
+            <View className="flex flex-row items-center mt-5 justify-between border w-full h-[50px] rounded-md border-third overflow-hidden py-4 pl-4">
               <MaterialIcons name="lock-outline" size={17} color="#b1b6c8" />
               <TextInput
                 value={password}
@@ -102,29 +102,6 @@ const Login = () => {
               handlePress={handleLogin}
               content="Sign In"
             />
-          </View>
-          <View className="w-full flex-row justify-center items-center pb-4">
-            <View className="h-[1px] mr-4 w-[40%] bg-third"></View>
-            <Text className="text-third font-bold">OR</Text>
-            <View className="h-[1px] ml-4 w-[40%] bg-third"></View>
-          </View>
-          <View className="flex flex-col w-full">
-            <TouchableOpacity className="relative h-[50px] flex-row items-center justify-center border border-third w-full rounded-md">
-              <Image
-                source={googleImg}
-                className="w-[28px] h-[28px] absolute left-6"
-                alt=""
-              />
-              <Text className="text-third">Login with Google</Text>
-            </TouchableOpacity>
-            <TouchableOpacity className="relative h-[50px] mt-2 flex-row items-center justify-center border border-third w-full rounded-md">
-              <Image
-                source={facebookImg}
-                className="w-[19px] h-[19px] absolute left-7"
-                alt=""
-              />
-              <Text className="text-third">Login with Facebook</Text>
-            </TouchableOpacity>
           </View>
           <View className="flex flex-col items-center pt-5">
             <Text className="text-primary font-bold">Forgot Password? </Text>
